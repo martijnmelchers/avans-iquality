248dc2a869037eb7075b40e11e69df141fe67e8f
Martijn <martijn@melchers.nu>
2020-02-01 14:37:36 +0100

    Added initial projects

---
+++ src/DomainServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IQuality.DomainServices
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IQuality.Models
+{
+    public class Class1
+    {
+    }
+}
