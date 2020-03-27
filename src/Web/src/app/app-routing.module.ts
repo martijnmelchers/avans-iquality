5b6664e1e83b30480b00bf5a48a92da28d97d1af
SaschaMendel <saschamendel2016@gmail.com>
2020-03-27 19:56:12 +0100

    some models

--- src/Web/src/app/app-routing.module.ts
+++ src/Web/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InviteComponent } from "./invite/invite.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "invite", component: InviteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/Web/src/app/shared/Models/address.ts
@@ -0,0 +1,8 @@
+export class Address {
+  zipCode: string;
+    streetName: string;
+    houseNumber: number;
+    suffix: string;
+    city: string;
+    country: string;
+}
