82f4906ccc2623857cf74d7445aefe01e8f86c59
Martijn <martijn@melchers.nu>
2020-05-04 15:33:36 +0200

    Some small fixes
    Signed-off-by: Martijn <martijn@melchers.nu>

--- src/DomainServices/Dialogflow/IntentHandlers/ActionIntentHandler.cs
+++ src/DomainServices/Dialogflow/IntentHandlers/ActionIntentHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System.Threading.Tasks;
 using Google.Cloud.Dialogflow.V2;
 using IQuality.DomainServices.Dialogflow.Interfaces;
+using IQuality.DomainServices.Interfaces;
 using IQuality.Models.Chat;
 using IQuality.Models.Chat.Messages;
+using IQuality.Models.Forms;
 using IQuality.Models.Helpers;
 
 namespace IQuality.DomainServices.Dialogflow.IntentHandlers
@@ -10,12 +12,32 @@ namespace IQuality.DomainServices.Dialogflow.IntentHandlers
     [Injectable]
     public class ActionIntentHandler : IActionIntentHandler
     {
+        private readonly IGoalService _goalService;
+        
+        public ActionIntentHandler(IGoalService goalService)
+        {
+            _goalService = _goalService;
+        }
         public async Task<BotMessage> HandleClientIntent(PatientChat chat, string userInput, QueryResult queryResult)
         {
             var response = new BotMessage();
 
             switch (chat.Intent.Name)
             {
+                case "create_action":
+                    chat.Intent.Name = "select_goal_for_action";
+
+                    response.Content = queryResult.FulfillmentText;
+                    response.ResponseType = ResponseType.Text;
+                    
+                    break;
+                
+                case "select_goal_for_action":
+                    if (!await _goalService.GoalExists(chat.Id, userInput))
+                        response.RespondText("I'm sorry but I can't find that goal, check the spelling and try again.");
+                    
+
+                    break;
             }
 
             return response;
