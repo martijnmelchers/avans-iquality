9806ca1cb70b0f6e9c425a21ace47856785859ad
Darjush Kolahi <d.kol.dev@hotmail.com>
2020-05-14 15:15:40 +0200

    added actioncontroller and getactiontypes httpget
    Co-Authored-By: masuddr <masuddr@users.noreply.github.com>

--- src/Api/Controllers/ActionController.cs
+++ src/Api/Controllers/ActionController.cs
@@ -3,29 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IQuality.Api.Extensions;
-using IQuality.DomainServices.Interfaces;
 using IQuality.Models;
 using IQuality.Models.Authentication;
-using IQuality.Models.Authentication.Settings;
-using IQuality.Models.Chat;
-using IQuality.Models.Goals;
 using IQuality.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Raven.Client.Documents.Session;
 
 namespace IQuality.Api.Controllers
 {
-    [Route("/patient")]
-    public class PatientController : RavenApiController
+    [Route("[controller]")]
+    public class ActionController : RavenApiController
     {
         private readonly IAsyncDocumentSession _session;
         private readonly IActionService _actionService;
+        private readonly IActionTypesService _actionTypesService;
 
-        public PatientController(IAsyncDocumentSession session, IActionService actionService) : base(session)
+        public ActionController(IAsyncDocumentSession session, IActionService actionService, IActionTypesService actionTypesService) : base(session)
         {
             _session = session;
             _actionService = actionService;
+            _actionTypesService = actionTypesService;
+        }
+
+        [HttpGet, Route("getactiontypes"), Authorize(Roles = "Patient, Doctor")]
+        public IActionResult GetActionTypes()
+        {
+            return Ok(_actionTypesService.GetActionTypes());
         }
 
         // setactionreminder duratie actionid
