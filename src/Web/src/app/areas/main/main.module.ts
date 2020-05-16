047274ba375e348a9ef2e4e8a198c8bacff6364b
HuseyinCaliskan <huseyincaliskan32@gmail.com>
2020-05-16 16:34:42 +0200

    fixed compile error

--- src/Web/src/app/areas/main/main.module.ts
+++ src/Web/src/app/areas/main/main.module.ts
@@ -4,13 +4,13 @@ import {CommonModule} from '@angular/common';
 import {MainRoutingModule} from './main-routing.module';
 import {HomeComponent} from './home/home.component';
 import {
-    ButtonModule,
-    GridModule,
-    InputModule,
-    ListModule, ModalModule, NotificationModule, NotificationService, SearchModule, PlaceholderModule, PlaceholderService,
-    StructuredListModule,
-    TableModule, TagModule,
-    TilesModule, ToggleModule,
+  ButtonModule,
+  GridModule,
+  InputModule,
+  ListModule, ModalModule, NotificationModule, NotificationService, SearchModule, PlaceholderModule, PlaceholderService,
+  StructuredListModule,
+  TableModule, TagModule,
+  TilesModule, ToggleModule,
 } from 'carbon-components-angular';
 import {ChatComponent} from './chat/chat.component';
 import {MessageComponent} from './chat/message/message.component';
@@ -26,36 +26,47 @@ import {
   TrashCanModule
 } from "@carbon/icons-angular";
 import {DoctorComponent} from "@IQuality/areas/main/doctor/doctor.component";
-import { ChatInstanceComponent } from './chat/chat-instance/chat-instance.component';
+import {ChatInstanceComponent} from './chat/chat-instance/chat-instance.component';
+import {BotMessageComponent} from "@IQuality/areas/main/chat/message/bot-message/bot-message.component";
+import {UserMessageComponent} from "@IQuality/areas/main/chat/message/user-message/user-message.component";
+import {ChartsModule} from "@carbon/charts-angular";
 
 @NgModule({
-  declarations: [HomeComponent, ChatComponent, MessageComponent, InviteComponent, ChatListComponent, DoctorComponent, ChatInstanceComponent, UserMessageComponent, BotMessageComponent],
-    imports: [
-        CommonModule,
-        MainRoutingModule,
-        TableModule,
-        GridModule,
-        InputModule,
-        ButtonModule,
-        ReactiveFormsModule,
-        TilesModule,
-        ListModule,
-        StructuredListModule,
-        AddModule,
-        FormsModule,
-        SendAltModule,
-        CloseModule,
-        ColumnModule,
-        TagModule,
-        ToggleModule,
-        DotMarkModule,
-        TrashCanModule,
-        DeleteModule,
-        NotificationModule,
-        ChartsModule,
-        SearchModule,
-
-
+  declarations: [
+    HomeComponent,
+    ChatComponent,
+    MessageComponent,
+    InviteComponent,
+    ChatListComponent,
+    DoctorComponent,
+    ChatInstanceComponent,
+    UserMessageComponent,
+    BotMessageComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    TableModule,
+    GridModule,
+    InputModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    TilesModule,
+    ListModule,
+    StructuredListModule,
+    AddModule,
+    FormsModule,
+    SendAltModule,
+    CloseModule,
+    ColumnModule,
+    TagModule,
+    ToggleModule,
+    DotMarkModule,
+    TrashCanModule,
+    DeleteModule,
+    NotificationModule,
+    ChartsModule,
+    SearchModule,
   ],
   providers: []
 })
