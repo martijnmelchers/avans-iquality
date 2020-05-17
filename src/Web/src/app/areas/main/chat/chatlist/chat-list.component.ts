93045287386965e98a0942119e0e6af713b8905f
Masud Rasa <m.rasa1@student.avans.nl>
2020-05-17 17:19:59 +0200

    notification front end
    Co-Authored-By: Darjush Kolahi <darjushkolahi@users.noreply.github.com>

--- src/Web/src/app/areas/main/chat/chatlist/chat-list.component.ts
+++ src/Web/src/app/areas/main/chat/chatlist/chat-list.component.ts
@@ -3,6 +3,8 @@ import {ChatService} from "@IQuality/core/services/chat.service";
 import {ChatContext} from "@IQuality/core/models/chat-context";
 import {NotificationService} from "carbon-components-angular";
 import {Message} from "@IQuality/core/models/messages/message";
+import { ApiService } from '@IQuality/core/services/api.service';
+import { Tip } from '@IQuality/core/models/tip';
 
 @Component({
   selector: 'app-chat-list',
@@ -13,15 +15,22 @@ import {Message} from "@IQuality/core/models/messages/message";
 export class ChatListComponent implements OnInit {
   chats: Array<ChatContext> = [];
   chatName: string;
+  notification: any = {};
 
-  constructor(public chatService: ChatService) {
+  constructor(public chatService: ChatService, private _api: ApiService) {
   }
 
-  ngOnInit(): void {
+  //TODO: Verplaatsen
+  async ngOnInit(): Promise<void> {
     this.chatService.getChats().then((response) => {
       this.chats = response;
 
     }, err => console.log(err));
+
+    await this._api.get<any>('/tip/getrandomtip').then(resp =>{
+      this.notification = resp;
+      
+    });
   }
 
   onBuddyChatCreate(isBuddyChat: boolean) {
